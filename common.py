24951ca1630c5643c6757e2b483ad247ee00d988
Vincent Cohadon <vincent.cohadon@etu.u-paris.fr>
2024-10-13 00:37:37 +0200

    Update csv

--- common.py
+++ common.py
@@ -195,7 +195,6 @@ class DropDownView(ui.View):
             name = f'{row.iloc[1]} {row.iloc[0]}'.title()
             if name not in member_names:
                 self.options.append(SelectOption(label=name, value=name, emoji='🎓'))
-        print(self.options[22])
         self.current_page = 1
         self.per_page = 25
 
