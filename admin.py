b2585870dff27443b1f886b63ba368d50e3e7ccd
Vincent Cohadon <vincent.cohadon@etu.u-paris.fr>
2024-10-14 19:40:25 +0200

    Change announce icon to user's avatar

--- admin.py
+++ admin.py
@@ -22,8 +22,8 @@ class Admin(commands.Cog):
     @app_commands.describe(title='Le titre de l\'annonce.', message='Le message à annoncer.')
     @app_commands.checks.has_any_role(1291503961139838987, 1293714448263024650)
     async def announce(self, ctx: Interaction, title: str, message: str):
-        embed = Embed(title=title, description=message.replace('\\n', '\n'), color=0x8B1538, timestamp=ctx.created_at)
-        embed.set_footer(text=f"Annoncé par {ctx.user.display_name}", icon_url=ctx.guild.icon.url)
+        embed = Embed(title=title, description=message.replace('\\n', '\n'), color=0x8B1538)
+        embed.set_footer(text=f"Annoncé par {ctx.user.display_name}", icon_url=ctx.user.avatar.url)
         await ctx.response.send_message('Quels rôles voulez-vous mentionner ?', view=DropdownView(self.bot.guilds[0], embed), ephemeral=True)
 
     @app_commands.command(description="Efface un nombre de messages.")
