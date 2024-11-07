cd0a30df448671ccb3a970e45a4e2fbfd1eea034
Vincent Cohadon <vincent.cohadon@etu.u-paris.fr>
2024-11-08 00:24:15 +0100

    test for reminder autocompletion

--- cogs/reminder.py
+++ cogs/reminder.py
@@ -1,8 +1,9 @@
-from discord import app_commands, Interaction, TextChannel, Embed
+from discord import app_commands, Interaction, Embed
 from discord.errors import NotFound
 from discord.ext import commands
 
 from datetime import datetime
+import re
 
 
 class Reminder(commands.Cog):
@@ -15,6 +16,15 @@ class Reminder(commands.Cog):
     
     def save_reminders(self):
         self.bot.config.set('reminders', self.reminders)
+    
+    async def channel_autocomplete(self, interaction: Interaction, current: str) -> list[app_commands.Choice[str]]:
+        category = self.bot.get_channel(1289244121004900487)
+
+        return [
+            app_commands.Choice(name=channel.name, value=channel.name)
+            for channel in category.text_channels
+            if current.lower() in re.sub(r':.+?:', '', channel.name.lower())
+        ]
 
     @app_commands.command(description="Etablit un rappel pour un événement.")
     @app_commands.describe(course="Choisir le cours.", date="Choisir la date de l'événement.", event="Nom de l'événement", modality="Modalité de l'événement")
@@ -24,7 +34,8 @@ class Reminder(commands.Cog):
         app_commands.Choice(name="edit", value="2"),
         app_commands.Choice(name="remove", value="3")
     ])
-    async def calendar(self, interaction: Interaction, option: app_commands.Choice[str], course: TextChannel, date: str, event: str, description: str = None, modality: str = None):
+    @app_commands.autocomplete(course=channel_autocomplete)
+    async def calendar(self, interaction: Interaction, option: app_commands.Choice[str], course: str, date: str, event: str, description: str = None, modality: str = None):
         try:
             if ' ' not in date:
                 date += ' 23:59'
@@ -41,7 +52,7 @@ class Reminder(commands.Cog):
 
             if option.value == "1":
                 reminder = {
-                    "name": course.name,
+                    "name": course,
                     "fields": [
                         {
                             "name": event,
@@ -54,22 +65,22 @@ class Reminder(commands.Cog):
 
                 if msg:
                     for embed in msg.embeds:
-                        if embed.title == course.name.upper():
+                        if embed.title == course.upper():
                             embed.add_field(name=f'__{event}__', value=(description + "\n\n" if description else "") + f'Echéance: {reminder_date}R>' + ("\n\n``" + modality + '``' if modality else ""), inline=False)
                             await msg.edit(embeds=msg.embeds)
                             break
                     else:
-                        embed = Embed(title=course.name.upper())
+                        embed = Embed(title=course.upper())
                         embed.add_field(name=f'__{event}__', value=(description + "\n\n" if description else "") + f'Echéance: {reminder_date}R>' + ("\n\n``" + modality + '``' if modality else ""), inline=False)
                         await msg.edit(embeds=msg.embeds + [embed])
                 else:
-                    embed = Embed(title=course.name.upper())
+                    embed = Embed(title=course.upper())
                     embed.add_field(name=f'__{event}__', value=(description + "\n\n" if description else "") + f'Echéance: {reminder_date}R>' + ("\n\n``" + modality + '``' if modality else ""), inline=False)
                     msg = await reminders_channel.send(embeds=[embed])
                     self.bot.config.set('reminders_message_id', msg.id)
 
                 for existing_reminder in self.reminders:
-                    if existing_reminder['name'] == course.name:
+                    if existing_reminder['name'] == course:
                         existing_reminder['fields'].append({
                             "name": event,
                             "date": reminder_date,
@@ -87,7 +98,7 @@ class Reminder(commands.Cog):
             elif option.value == "2":
                 if msg:
                     for embed in msg.embeds:
-                        if embed.title == course.name.upper():
+                        if embed.title == course.upper():
                             for index, field in enumerate(embed.fields):
                                 if field.name == f'__{event}__':
                                     embed.set_field_at(index, name=f'__{event}__', value=(description + "\n\n" if description else "") + f'Echéance: {reminder_date}R>' + ("\n\n``" + modality + '``' if modality else ""), inline=False)
@@ -102,7 +113,7 @@ class Reminder(commands.Cog):
                         return
 
                     for existing_reminder in self.reminders:
-                        if existing_reminder['name'] == course.name:
+                        if existing_reminder['name'] == course:
                             for field in existing_reminder['fields']:
                                 if field['name'] == event:
                                     field['date'] = reminder_date
@@ -120,7 +131,7 @@ class Reminder(commands.Cog):
             elif option.value == "3":
                 if msg:
                     for embed in msg.embeds:
-                        if embed.title == course.name.upper():
+                        if embed.title == course.upper():
                             for field in embed.fields:
                                 if field.name == f'__{event}__':
                                     embed.remove_field(embed.fields.index(field))
@@ -141,7 +152,7 @@ class Reminder(commands.Cog):
                         return
 
                     for existing_reminder in self.reminders:
-                        if existing_reminder['name'] == course.name:
+                        if existing_reminder['name'] == course:
                             existing_reminder['fields'] = [field for field in existing_reminder['fields'] if field['name'] != event]
                             if not existing_reminder['fields']:
                                 self.reminders.remove(existing_reminder)
@@ -149,7 +160,7 @@ class Reminder(commands.Cog):
 
                     self.save_reminders()
 
-                    await interaction.response.send_message(f"Rappel pour l'événement '{event}' du cours '{course.name}' supprimé.", ephemeral=True)
+                    await interaction.response.send_message(f"Rappel pour l'événement '{event}' du cours '{course}' supprimé.", ephemeral=True)
                 else:
                     await interaction.response.send_message("Aucun message de rappel trouvé.", ephemeral=True)
 
