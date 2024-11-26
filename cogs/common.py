c1fd7d61cc555eec0e7ad1d420fb65cdc8bf7eba
0xf1d0 <vincent.cohadon@etu.u-paris.fr>
2024-11-26 23:36:03 +0100

    test

--- cogs/common.py
+++ cogs/common.py
@@ -21,7 +21,7 @@ class Common(commands.Cog):
         """
 
         self.bot = bot
-        self.guild = bot.get_guild(CYBER)
+        self.guild = bot.fetch_guild(CYBER)
     
     def missing_member_names(self):
         names = {'FI': [], 'FA': []}
