93cabcf9a7766a3ce04badbcbf740f91296351b3
0xf1d0 <vincent.cohadon@etu.u-paris.fr>
2025-02-17 19:34:50 +0100

    First changes for new auth system

--- cogs/register.py
+++ cogs/register.py
@@ -34,7 +34,7 @@ class Register(commands.Cog):
 
         view = AuthenticationView(self.missing_members)
         await self.welcome_message.edit(content=self.bot.config.get('welcome_message'), view=view)
-        self.bot.add_view(view, message_id=WELCOME_CHANNEL.id)
+        self.bot.add_view(view, message_id=WELCOME_MESSAGE.id)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: Member):
@@ -50,9 +50,9 @@ class Register(commands.Cog):
         
         self.missing_members = self.missing_member_names()
 
-        view = AuthenticationView(self.missing_members)
+        """view = AuthenticationView(self.missing_members)
         await self.welcome_message.edit(content=self.bot.config.get('welcome_message'), view=view)
-        self.bot.add_view(view, message_id=WELCOME_CHANNEL.id)
+        self.bot.add_view(view, message_id=WELCOME_CHANNEL.id)"""
 
 
 async def setup(bot: commands.Bot):
