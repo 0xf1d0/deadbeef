86b0bc81e6925dde1452c2efb82b60a84a7d157d
0xf1d0 <vincent.cohadon@etu.u-paris.fr>
2025-02-18 01:18:39 +0100

    Big update with refactor. Request for testing

--- ui/announce.py
+++ ui/announce.py
@@ -14,8 +14,8 @@ class Announcement(ui.View):
         if self.mentions:
             value += ' ' + ' '.join(self.mentions)
         d = {'content': f'|| {value} ||', 'embed': self.embed}
-        await interaction.response.edit_message(view=None, delete_after=0)
         await interaction.response.send_message(**d, view=Confirm(**d), ephemeral=True)
+        await interaction.response.edit_message(view=None, delete_after=0)
         self.stop()
 
 
