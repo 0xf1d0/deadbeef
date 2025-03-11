bcb2aad14a7bbdbf6496ac050d71a5fd79978793
0xf1d0 <vincent.cohadon@etu.u-paris.fr>
2025-03-11 23:35:35 +0100

    Fix user management

--- ui/auth.py
+++ ui/auth.py
@@ -104,18 +104,23 @@ class Token(ui.Modal):
         
         # Mise à jour des rôles et permissions
         if self.role in [ROLE_FI, ROLE_FA]:
+            user.update({
+                'studentId': self.student_id,
+                'email': self.email,
+            })
             await interaction.user.add_roles(self.role, ROLE_M1)
             try:
                 await interaction.user.edit(nick=self.nick)
             except Forbidden:
                 pass
         else:
+            user['id'] = interaction.user.id
             await interaction.user.add_roles(ROLE_NOTABLE)
             try:
                 await interaction.user.edit(nick=self.nick)
             except Forbidden:
                 pass
-            
+
             # Mise à jour des permissions des cours
             if 'courses' in user:
                 guild = interaction.guild
@@ -129,7 +134,6 @@ class Token(ui.Modal):
 
 
 class StudentModal(ui.Modal, title="Authentification"):
-    email = ui.TextInput(label="Email", placeholder="prenom.nom@etu.u-paris.fr")
     student_id = ui.TextInput(label="Numéro étudiant", placeholder="12345678")
 
     async def on_submit(self, interaction: Interaction):
@@ -140,15 +144,6 @@ class StudentModal(ui.Modal, title="Authentification"):
             await interaction.response.send_message("Vous êtes déjà authentifié.", ephemeral=True)
             return
         
-        existing_email_user = next((u for u in users if u['email'] == self.email.value and u['id'] is not None), None)
-        if existing_email_user:
-            await interaction.response.send_message("Cet email a déjà été enregistré.", ephemeral=True)
-            return
-            
-        if not self.email.value.endswith('@etu.u-paris.fr'):
-            await interaction.response.send_message("Email non valide.", ephemeral=True)
-            return
-        
         # Vérification des listes FI/FA
         valid_user = None
         for data in [FI, FA]:
@@ -156,20 +151,19 @@ class StudentModal(ui.Modal, title="Authentification"):
             role = ROLE_FI if data == FI else ROLE_FA
             
             for row in data:
-                if (f"{row[headers.index('Email')]}@etu.u-paris.fr" == self.email.value and
-                    row[headers.index('N° étudiant')] == self.student_id.value):
-                    
+                if row[headers.index('N° étudiant')] == self.student_id.value:
                     valid_user = {
                         'role': role,
                         'prenom': row[headers.index('Prénom')],
-                        'nom': row[headers.index('Nom')]
+                        'nom': row[headers.index('Nom')],
+                        'email': f"{row[headers.index('Email')]}@etu.u-paris.fr"
                     }
                     break
             if valid_user:
                 break
 
         if not valid_user:
-            await interaction.response.send_message("Email ou numéro étudiant invalide.", ephemeral=True)
+            await interaction.response.send_message("Numéro étudiant invalide.", ephemeral=True)
             return
         
         # Vérification du cooldown
@@ -187,21 +181,19 @@ class StudentModal(ui.Modal, title="Authentification"):
             users.append(current_user)
             
         current_user.update({
-            'email': self.email.value,
-            'studentId': self.student_id.value,
             'last_auth_request': datetime.now().isoformat()
         })
         ConfigManager.set('users', users)
         
         send_email(
             ConfigManager.get('email_object'),
-            ConfigManager.get('email_body').format(create_jwt(self.email.value)),
-            self.email.value
+            ConfigManager.get('email_body').format(create_jwt(valid_user['email'])),
+            valid_user['email']
         )
 
         await interaction.followup.send(
-            f"Mail envoyé à {self.email.value}",
-            view=Feedback(self.email.value, f"{valid_user['prenom']} {valid_user['nom']}".title(), 
+            f"Mail envoyé à {valid_user['email']}",
+            view=Feedback(valid_user['email'], f"{valid_user['prenom']} {valid_user['nom']}".title(), 
                           valid_user['role'], self.student_id.value),
             ephemeral=True
         )
